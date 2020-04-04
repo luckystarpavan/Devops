86af028e297fefa03205db1d3bdebfb1fbc14b21
luckystarpavan <48612903+luckystarpavan@users.noreply.github.com>
2020-04-04 22:37:00 +0530

    cleared the errors

--- terraform/practice/ubuntu_launch.tf
+++ terraform/practice/ubuntu_launch.tf
@@ -12,3 +12,12 @@ provider "aws" {
 resource "aws_instance" "example" {
    ami          = "ami-07ebfd5b3428b6f4d"
    instance_type = "t2.micro"
+   }
+
+
+
+
+################################################################
+#by adding below this ( key_name = "MyVPC-DFW-office" ) in resource, we will select the particular key pair for an instance.
+
+ 
