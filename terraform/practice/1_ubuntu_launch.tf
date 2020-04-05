e0d13b7e791303a712504675a0144baaddb64fdc
luckystarpavan <48612903+luckystarpavan@users.noreply.github.com>
2020-04-05 16:15:08 +0530

    added Tag_name

--- terraform/practice/1_ubuntu_launch.tf
+++ terraform/practice/1_ubuntu_launch.tf
@@ -12,7 +12,11 @@ provider "aws" {
 resource "aws_instance" "example" {
    ami          = "ami-07ebfd5b3428b6f4d"
    instance_type = "t2.micro"
-   }
+tags =  {
+     Name = "Terraform_1"
+       }
+}
+
 
 
 
